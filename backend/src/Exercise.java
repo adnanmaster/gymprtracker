37ab1ee8ba44baff3f7aaebbf9999435b732cbfc
haldeadn <haldeadn@students.zhaw.ch>
2025-11-10 11:01:12 +0100

    setter and getter for Exercise

--- backend/src/Exercise.java
+++ backend/src/Exercise.java
@@ -1,12 +1,36 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise {
     private String name;
     private String description;
     private ExerciseType type;
-    private boolean Unilateral = false;
+    private boolean unilateral = false;
     private double PRweight;
     private int PRreps;
-    private List<Entry> history;
+    private List<Entry> history = new ArrayList<>();
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setType(ExerciseType type) {
+        this.type = type;
+    }
+
+    public void setUnilateral(boolean unilateral) {
+        this.unilateral = unilateral;
+    }
+
+    public double getPRweight() {
+        return PRweight;
+    }
+
+    public int getPRreps() {
+        return PRreps;
+    }
 }
