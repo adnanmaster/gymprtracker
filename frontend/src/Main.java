f3c89b8843b9c1bcd609d117620c4498d55c354b
haldeadn <haldeadn@students.zhaw.ch>
2025-11-13 09:37:39 +0100

    JavaFX Module "Frontend" added

--- frontend/src/Main.java
+++ frontend/src/Main.java
@@ -1,4 +1,4 @@
-public static void main(String [] args) {
+public static void main(String[] args) {
     Exercise test = new Exercise();
     test.setName("test1234567890");
     System.out.println(test.getName());
